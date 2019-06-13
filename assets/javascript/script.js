e082184f10c520d1a521450835b9dc75930948f9
Avinash Singh <avinash.dhillon@gmail.com>
2019-06-12 19:38:17 -0500

    created HTML skeleton layout

---
+++ assets/javascript/script.js
@@ -0,0 +1,16 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAJwgHwShAhZfBRl50JfFzKfC8ohXQ1li8",
+    authDomain: "rps-multiplayer-4804c.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-4804c.firebaseio.com",
+    projectId: "rps-multiplayer-4804c",
+    storageBucket: "rps-multiplayer-4804c.appspot.com",
+    messagingSenderId: "438083069166",
+    appId: "1:438083069166:web:9ce301a0b6cc81d6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+var dataRef = firebase.database();
